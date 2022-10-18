50833082d06d126769b24354be8c2728e58568e3
Renat <renatakhmedyanov@gmail.com>
2022-10-18 15:09:13 +0400

    Add Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+Console.Write("Введите целое число: ");
+int input = int.Parse(Console.ReadLine()!);
+
+bool HasThreeDigits(int input)
+{
+    return input < 100;
+}
+void GetThirdDigit(int input)
+{
+    while (input > 1000)
+    {
+        input = input / 10;
+    }
+    int result = input % 10;
+    Console.WriteLine("Третья цифра введенного числа: " + result);
+}
+
+if (HasThreeDigits(input))
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+else
+{
+    GetThirdDigit(input);
+}
+
+
