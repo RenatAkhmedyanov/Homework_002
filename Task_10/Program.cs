689eb795185269c87eb95d6af375f44701952859
Renat <renatakhmedyanov@gmail.com>
2022-10-18 12:55:29 +0400

    Add Task 10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+
+Console.Write("Введите трехзначное число: ");
+int input = int.Parse(Console.ReadLine()!);
+
+void GetSecondDigit(int input)
+{
+    if (IsCorrectInput(input))
+    {
+        Console.WriteLine("Ошибка ввода. Введите трехзначное число!");
+    }
+    else
+    {
+        int secondDigit = (input / 10) % 10;
+        Console.WriteLine("Вторая цифра введеного числа: " + secondDigit);
+    }
+}
+bool IsCorrectInput(int input)
+{
+    return input < 100 || input > 999;
+}
+GetSecondDigit(input);
