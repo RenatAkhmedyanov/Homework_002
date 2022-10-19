e05278823b3ce5a916188ff2255f1f99edc24059
RenatAkhmedyanov <115239782+RenatAkhmedyanov@users.noreply.github.com>
2022-10-19 21:16:17 +0400

    Add correct check for numbers 11, 12, 13, 14

--- Task_11_2/Program.cs
+++ Task_11_2/Program.cs
@@ -1,22 +1,31 @@
-﻿Console.WriteLine("Программа, считывающая с пользовательского ввода целое число n (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом 'программист'.");
+Console.WriteLine("Программа, считывающая с пользовательского ввода целое число n (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом 'программист'.");
 
 Console.Write("Введите количество программистов: ");
 int input = int.Parse(Console.ReadLine()!);
 int check = input % 10;
+int secondCheck = input % 100; //для чисел, который заканчиваются на 11, 12, 13, 14
 
 void HasCorrectEnding(int input, int check)
 {
-    if (check == 1)
-    {
-        Console.WriteLine("В комнате " + input + " программист.");
-    }
-    if (check >= 2 && check <= 4)
-    {
-        Console.WriteLine("В комнате " + input + " программиста.");
-    }
-    if (check >= 5)
+    if (secondCheck > 10 && secondCheck < 20)
     {
         Console.WriteLine("В комнате " + input + " программистов.");
     }
+    else
+    {
+        if (check == 1)
+        {
+            Console.WriteLine("В комнате " + input + " программист.");
+        }
+        if (check >= 2 && check <= 4)
+        {
+            Console.WriteLine("В комнате " + input + " программиста.");
+        }
+        if (check >= 5)
+        {
+            Console.WriteLine("В комнате " + input + " программистов.");
+        }
+    }
+
 }
 HasCorrectEnding(input, check);
